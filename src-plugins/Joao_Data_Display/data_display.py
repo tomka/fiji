29943483f573ccfb72a650deb084ad63c7635501
Tom Kazimiers <tom@voodoo-arts.net>
2011-12-19 15:02:01 +0100

    Joao data display: better GUI organization

--- src-plugins/Joao_Data_Display/data_display.py
+++ src-plugins/Joao_Data_Display/data_display.py
@@ -1,6 +1,23 @@
+"""
+Small script to display various experiment data sets with
+the help of Fiji.
+
+Tom Kazimiers, December 2011
+"""
+
 import sys
 
-from javax.swing import JScrollPane, JPanel, JComboBox, JLabel, JFrame
+from javax.swing import JPanel, JList, JLabel, JFrame, JButton
+from java.awt import BorderLayout, GridLayout
+
+# Log a method
+def log(msg):
+	IJ.log(msg)
+
+# Log a method and exit with return value 1
+def exit(msg):
+	log(msg)
+	sys.exit(1)
 
 # A named protein with a list of markers
 class Protein:
@@ -20,27 +37,78 @@ class Project:
 		self.name = name
 		self.proteins = proteins
 
-def log(msg):
-	IJ.log(msg)
+# A GUI that supports multiple screens
+class GUI:
+	def __init__(self, project):
+		self.project = project
+		self.frame = None
+		self.selectionPanel = None
+		self.lists = []
+		self.init()
 
-def exit(msg):
-	log(msg)
-	sys.exit(1)
+	def init(self):
+		frame = JFrame( "Data Display for " + self.project.name )
+		#frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout( BorderLayout() )
+		# The selection panel covers everything for protein/marker selection
+		selectionPanel = JPanel( BorderLayout() )
+		proteinPanel = JPanel( GridLayout( 0, len( self.project.proteins ) ) )
+		# add a JList for all the proteins
+		for p in self.project.proteins:
+			panel = JPanel( BorderLayout() )
+			panel.add( JLabel(p.name), BorderLayout.NORTH )
+			markerList = JList(p.markers)
+			self.lists.append( markerList )
+			markerList.valueChanged = self.select
+			panel.add( markerList, BorderLayout.CENTER );
+			proteinPanel.add( panel )
+		#frame.getContentPane().add(JScrollPane(all))
+		continueButton = JButton("Show data", actionPerformed=self.showData)
+		selectionPanel.add( JLabel( "Please select a combination" ), BorderLayout.NORTH )
+		selectionPanel.add( proteinPanel, BorderLayout.CENTER )
+		selectionPanel.add( continueButton, BorderLayout.SOUTH )
+		frame.add( selectionPanel, BorderLayout.CENTER )
+		frame.pack()
+		frame.setSize( 400, 300 )
+		frame.setVisible( False )
+		# store as fields
+		self.frame = frame
+		self.selectionPanel = selectionPanel
 
-# GUI
-def initGUI(project):
-	frame = JFrame("Data Display for " + project.name)
-	#frame.getContentPane().add(JScrollPane(all))
-	frame.pack()
-	frame.setVisible(True)
+	def show(self):
+		self.frame.setVisible( True )
+
+	def doNothing(self, event):
+		return
+
+	def select(self, event):
+		# react only to final selection event
+		if event.getValueIsAdjusting():
+			return
+		# remove all event handlers
+		for l in self.lists:
+			l.valueChanged = self.doNothing
+		# get current selection and update lists
+		srcList = event.source
+		marker = srcList.selectedValue
+		log( "Changing selection to " + marker )
+		for l in self.lists:
+			if l is not srcList:
+				l.clearSelection()
+		# add all event handlers
+		for l in self.lists:
+			l.valueChanged = self.select
+
+	def showData(self, event):
+		log( "Showing data for " )
 
 # Creates projects based on a name
 def loadProject(name):
 	project = None
 	if name == "Joao":
-		prot1 = Protein("WT", ["SpiderGFP", "EcadGFP", "SASVenus", "LachesinGFP"])
-		prot2 = Protein("crb11A22", ["SpiderGFP, EcadGFP, SASVenus"])
-		project = Project(name, [prot1, prot2])
+		prot1 = Protein( "WT", ["SpiderGFP", "EcadGFP", "SASVenus", "LachesinGFP"] )
+		prot2 = Protein( "crb11A22", ["SpiderGFP", "EcadGFP", "SASVenus"] )
+		project = Project( name, [prot1, prot2] )
 	else:
 		log("Could not find definition for project with name " + name)
 	return project
@@ -51,7 +119,8 @@ def main():
 	project = loadProject("Joao")
 	if project is None:
 		exit("Loading failed, exiting")
-	initGUI(project)
+	gui = GUI(project)
+	gui.show()
 
 # call main
 if __name__ == "__main__":
